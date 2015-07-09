f1c26d7f0a01f72ba9bbcc44e4a1466ab6388ecf
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-07-09 10:41:11 +0200

    chore: add history file generation

--- configure.js
+++ configure.js
@@ -28,35 +28,36 @@ var lopts = `\\
     -lfolly \\
     -lglog`
 
-generateProject(function (_) {
-  "use strict"
-  _.clang = (src, ...deps) => {
-    var command = (_) => `clang++ -g ${copts} -c ${_.source} -o ${_.product}`
-    var product = (_) => `${_.source.replace(/\..*/, '.o')}`
-    _.compileFiles(...([command, product, src].concat(deps)))
-  }
+generateProject(function(_) {
+    "use strict"
+    _.clang = (src, ...deps) => {
+        var command = (_) => `clang++ -g ${copts} -c ${_.source} -o ${_.product}`
+        var product = (_) => `${_.source.replace(/\..*/, '.o')}`
+        _.compileFiles(...([command, product, src].concat(deps)))
+    }
 
-  _.clangExe = (body) => {
-    var command = (_) => `clang++ -g ${lopts} ${_.sources.join(' ')} -o ${_.product}`
-    var product = () => `clang-${uid(4)}.x`
-    _.reduceFiles(command, product, body)
-  }
+    _.clangExe = (body) => {
+        var command = (_) => `clang++ -g ${lopts} ${_.sources.join(' ')} -o ${_.product}`
+        var product = () => `clang-${uid(4)}.x`
+        _.reduceFiles(command, product, body)
+    }
 
-	_.collect("docs", _ => {
-		_.cmd("./node_modules/.bin/git-hist readme.md")
-	});
+    _.collect("docs", _ => {
+        _.cmd("./node_modules/.bin/git-hist readme.md")
+		_.cmd("hub cm 'chore: update history file'")
+    });
 
 
-  _.collectSeq("all", function (_) {
-    _.toFile("bin/teasy.x", (_) => {
-      _.clangExe((_) => {
-          _.clang("src/*.cpp", "src/*.hpp", "lib3rd/**/*.{h,hpp}");
-          _.clang("lib3rd/CF++/source/*.cpp", "lib3rd/CF++/include/**/*.h");
-          _.clang("lib3rd/docopt/docopt.cpp", "lib3rd/docopt/docopt.h");
-          _.clang("lib3rd/json11/json11.cpp", "lib3rd/json11/json11.hpp");
-		  _.clang("lib3rd/cppformat/*.cc", "lib3rd/cppformat/*.h");
-      })
+    _.collectSeq("all", function(_) {
+        _.toFile("bin/teasy.x", (_) => {
+            _.clangExe((_) => {
+                _.clang("src/*.cpp", "src/*.hpp", "lib3rd/**/*.{h,hpp}");
+                _.clang("lib3rd/CF++/source/*.cpp", "lib3rd/CF++/include/**/*.h");
+                _.clang("lib3rd/docopt/docopt.cpp", "lib3rd/docopt/docopt.h");
+                _.clang("lib3rd/json11/json11.cpp", "lib3rd/json11/json11.hpp");
+                _.clang("lib3rd/cppformat/*.cc", "lib3rd/cppformat/*.h");
+            })
+        })
+        _.cmd("chmod +x bin/teasy.x")
     })
-    _.cmd("chmod +x bin/teasy.x")
-  })
 })
